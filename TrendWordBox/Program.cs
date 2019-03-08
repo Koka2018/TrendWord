80885266c513d51c1a9f9db8339e6466c5a8a91c
koka <nillish2003@yahoo.co.jp>
2019-03-09 00:29:46 +0900

    更新

--- TrendWordBox/Program.cs
+++ TrendWordBox/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrendWordGear;
 
 namespace TrendWordBox
 {
@@ -10,6 +11,10 @@ namespace TrendWordBox
     {
         static void Main(string[] args)
         {
+            var ctrl = new TrendWordCtrl();
+
+            var tbl = ctrl.GetWordCntTbl("「ガンダム」とはアニメ作品『機動戦士ガンダム』劇中に登場したモビルスーツ（ロボット）・「ガンダム」のことだが、「ガンプラ」という名称を広義に用いる場合は単にガンダム一体だけを指すのではなく、「ガンダムシリーズ」全体に登場する他のモビルスーツやモビルアーマー等と呼ばれる兵器、および艦船など、商品としてプラモデル化されているもの全ての総称として用いられる。 ");
+            Console.ReadLine();
         }
     }
 }
---
+++ TrendWordGear/TrendWordCtrl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMeCab;
+
+namespace TrendWordGear
+{
+    public class TrendWordCtrl
+    {
+        #region 定数
+
+        private enum EnumMeCabIdx
+        {
+            Type = 0,       // 品詞
+            DetailType1,   // 品詞細分類1
+            DetailType2,   // 品詞細分類2
+            DetailType3,   // 品詞細分類3
+            Adapted,        // 活用形
+            AdaptType,     // 活用型
+            Basic,             // 原形
+            WayOfRead,    // 読み
+            Pronunciation, // 発音
+        }
+
+        #endregion
+
+        #region メンバ変数
+
+        private MeCabTagger mTagger = MeCabTagger.Create();
+
+        #endregion
+
+        public TrendWordCtrl()
+        {
+        }
+
+        #region メソッド
+
+        public Dictionary<string, int> GetWordCntTbl(string text)
+        {
+            var tbl = new Dictionary<string, int>();
+
+            var node = mTagger.ParseToNode(text);
+            while(node != null)
+            {
+                var sp_Feature = node.Feature.Split(',');
+                if (sp_Feature[(int)EnumMeCabIdx.Type] == "名詞")
+                {
+                    Console.WriteLine(node.Surface + " : " + sp_Feature[(int)EnumMeCabIdx.Basic]);
+                }
+                node = node.Next;
+            }
+
+            return tbl;
+        }
+
+        #endregion
+    }
+}
