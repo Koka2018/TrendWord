005cd00d22ce43b6ecf807b338a5466f8e8c0b25
koka <nillish2003@yahoo.co.jp>
2019-03-09 14:11:59 +0900

    トークン情報クラスを追加

---
+++ TrendWordGear/Model/TokenData.cs
@@ -0,0 +1,93 @@
+﻿namespace TrendWordGear.Model
+{
+    public class TokenData
+    {
+        #region enum
+
+        private enum EnumMeCabIdx
+        {
+            Type = 0,       // 品詞
+            DetailType1,   // 品詞細分類1
+            DetailType2,   // 品詞細分類2
+            DetailType3,   // 品詞細分類3
+            Adapted,        // 活用形
+            AdaptType,     // 活用型
+            Basic,             // 原形
+            WayOfRead,    // 読み
+            Pronunciation, // 発音
+        }
+
+        #endregion
+
+        #region プロパティ
+
+        ///<summary> Word </summary>
+        public string Word { get; private set; } = string.Empty;
+        ///<summary> Feature </summary>
+        public string Feature { get; private set; } = string.Empty;
+
+        ///<summary> 品詞 </summary>
+        public string Type { get; private set; } = string.Empty;
+        ///<summary> 品詞細分類1 </summary>
+        public string DetailType1 { get; private set; } = string.Empty;
+        ///<summary> 品詞細分類2 </summary>
+        public string DetailType2 { get; private set; } = string.Empty;
+        ///<summary> 品詞細分類3 </summary>
+        public string DetailType3 { get; private set; } = string.Empty;
+        ///<summary> 活用形 </summary>
+        public string AdaptMethod { get; private set; } = string.Empty;
+        ///<summary> 活用型 </summary>
+        public string AdaptType { get; private set; } = string.Empty;
+        ///<summary> 原形 </summary>
+        public string BasicWord { get; private set; } = string.Empty;
+        ///<summary> 読み </summary>
+        public string WayOfRead { get; private set; } = string.Empty;
+        ///<summary> 発音 </summary>
+        public string Pronunciation { get; private set; } = string.Empty;
+
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="surface">Surface</param>
+        /// <param name="feature">Feature</param>
+        public TokenData(string surface, string feature)
+        {
+            Word = surface;
+            Feature = feature;
+
+            ParseFeature(feature);
+        }
+
+        #region メソッド
+
+        /// <summary>
+        /// Featureのパース
+        /// </summary>
+        /// <param name="feature">Feature</param>
+        private void ParseFeature(string feature)
+        {
+            var sp_Feature = feature.Split(',');
+
+            for (int col = 0; col < sp_Feature.Length; col++)
+            {
+                switch (col)
+                {
+                    case 0: Type = sp_Feature[col]; break;
+                    case 1: DetailType1 = sp_Feature[col]; break;
+                    case 2: DetailType2 = sp_Feature[col]; break;
+                    case 3: DetailType3 = sp_Feature[col]; break;
+                    case 4: AdaptMethod = sp_Feature[col]; break;
+                    case 5: AdaptType = sp_Feature[col]; break;
+                    case 6: BasicWord = sp_Feature[col]; break;
+                    case 7: WayOfRead = sp_Feature[col]; break;
+                    case 8: Pronunciation = sp_Feature[col]; break;
+                    default: break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- TrendWordGear/TrendWordCtrl.cs
+++ TrendWordGear/TrendWordCtrl.cs
@@ -1,57 +1,47 @@
-﻿using System;
+﻿using NMeCab;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NMeCab;
+using TrendWordGear.Model;
 
 namespace TrendWordGear
 {
     public class TrendWordCtrl
     {
-        #region 定数
-
-        private enum EnumMeCabIdx
-        {
-            Type = 0,       // 品詞
-            DetailType1,   // 品詞細分類1
-            DetailType2,   // 品詞細分類2
-            DetailType3,   // 品詞細分類3
-            Adapted,        // 活用形
-            AdaptType,     // 活用型
-            Basic,             // 原形
-            WayOfRead,    // 読み
-            Pronunciation, // 発音
-        }
-
-        #endregion
-
         #region メンバ変数
 
+        ///<summary> MeCabTagger </summary>
         private MeCabTagger mTagger = MeCabTagger.Create();
 
         #endregion
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public TrendWordCtrl()
         {
         }
 
         #region メソッド
 
-        public Dictionary<string, int> GetWordCntTbl(string text)
+        /// <summary>
+        /// トークンテーブル取得処理
+        /// </summary>
+        /// <param name="text">テキスト</param>
+        /// <returns>トークンテーブル</returns>
+        public Dictionary<string, List<TokenData>> GetTokenTbl(string text)
         {
-            var tbl = new Dictionary<string, int>();
+            var tbl = new Dictionary<string, List<TokenData>>();
 
             var node = mTagger.ParseToNode(text);
             while(node != null)
             {
-                var sp_Feature = node.Feature.Split(',');
-                if (sp_Feature[(int)EnumMeCabIdx.Basic] != "*"
-                    && node.Surface != sp_Feature[(int)EnumMeCabIdx.Basic])
+                var token = new TokenData(node.Surface, node.Feature);
+
+                if(tbl.ContainsKey(token.BasicWord) == false)
                 {
-                    Console.WriteLine(node.Surface + " : " + sp_Feature[(int)EnumMeCabIdx.Basic]
-                                                + node.Feature.Replace(",", "\t"));
+                    tbl[token.BasicWord] = new List<TokenData>();
                 }
+                tbl[token.BasicWord].Add(token);
+
                 node = node.Next;
             }
 
