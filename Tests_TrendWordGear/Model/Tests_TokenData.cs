d566b0db1704ec157e5d91eb48913a988286ac74
koka <nillish2003@yahoo.co.jp>
2019-03-31 02:49:09 +0900

    テストプロジェクトを追加

---
+++ Tests_TrendWordGear/Model/Tests_TokenData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendWordGear.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendWordGear.Model.Tests
+{
+    [TestClass()]
+    public class Tests_TokenData
+    {
+        [TestMethod()]
+        public void Test_TokenData()
+        {
+            Assert.Fail();
+        }
+    }
+}
