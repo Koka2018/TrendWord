4b75a193ab970c08e161516993f08beedf2c1723
koka <nillish2003@yahoo.co.jp>
2019-04-01 00:18:02 +0900

    更新

---
+++ TrendWordGear/Logic/AnalyzeLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using WordGear.Model;
+
+namespace WordGear.Logic
+{
+    public static class AnalyzeLogic
+    {
+        /// <summary>
+        /// 品詞指定によるトークンリスト抽出処理
+        /// </summary>
+        /// <param name="tokenList">トークンリスト</param>
+        /// <param name="type">品詞</param>
+        /// <returns>抽出したトークン</returns>
+        public static List<TokenData> ExtractTokenType(List<TokenData> tokenList, string type)
+        {
+            var extractTokenList = new List<TokenData>();
+            foreach (var token in tokenList)
+            {
+                if (token.Type == type)
+                {
+                    extractTokenList.Add(token);
+                }
+            }
+
+            return extractTokenList;
+        }
+
+        /// <summary>
+        /// 品詞指定によるトークンテーブル抽出処理
+        /// </summary>
+        /// <param name="tokenTbl">トークンテーブル</param>
+        /// <param name="type">品詞</param>
+        /// <returns>抽出したトークンテーブル</returns>
+        public static Dictionary<string, List<TokenData>> ExtractTokenType(Dictionary<string, List<TokenData>> tokenTbl, string type)
+        {
+            var extractTokenTbl = new Dictionary<string, List<TokenData>>();
+
+            foreach (var key in tokenTbl.Keys)
+            {
+                if (tokenTbl[key][0].Type != type) { continue; }
+                extractTokenTbl[key] = new List<TokenData>(tokenTbl[key]);
+            }
+
+            return extractTokenTbl;
+        }
+    }
+}
---
+++ TrendWordGear/Logic/ParagraphLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WordGear.Logic
+{
+    public static class ParagraphLogic
+    {
+        /// <summary>
+        /// 文の切り出し処理
+        /// </summary>
+        /// <param name="paragraph">文章</param>
+        /// <returns>文リスト</returns>
+        public static List<string> SplitParagraph(string paragraph)
+        {
+            var sentenceList = new List<string>(paragraph.Split('。'));
+
+            return sentenceList;
+        }
+    }
+}
