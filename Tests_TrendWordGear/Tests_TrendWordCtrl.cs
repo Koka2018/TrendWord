f79a51cada86a8f2b72f3f080677d01b1f62134e
koka <nillish2003@yahoo.co.jp>
2019-03-31 14:14:52 +0900

    不要な行を削除

--- Tests_TrendWordGear/Tests_TrendWordCtrl.cs
+++ Tests_TrendWordGear/Tests_TrendWordCtrl.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TrendWordGear;
-using TrendWordGear.Model;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TrendWordGear.Tests
 {
